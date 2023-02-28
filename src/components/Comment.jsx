db50a0f62fc95c34fa39f4665aea0096d7d15df6
Pedro Germano <pedrogermano232@gmail.com>
2023-02-28 01:33:31 -0300

    component comment done

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -18,7 +18,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom Devon, parabéns!! 👏👏</p>
