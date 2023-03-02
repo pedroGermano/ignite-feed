5bf3c58f167705781b60574ab1945fd696dcfed5
Pedro Germano <pedrogermano232@gmail.com>
2023-03-02 13:26:31 -0300

    Start refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import { Header } from "./components/Header";
-import { Post } from "./components/Post";
+import { Header } from './components/Header';
+import { Post } from './components/Post';
 
-import './global.css'
 import styles from './App.module.css';
-import { Sidebar } from "./components/Sidebar";
+import { Sidebar } from './components/Sidebar';
+import './global.css';
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatar: {
+        avatarUrl: 'https://github.com/diego3g.png',
+        name: 'Diego Fernandes',
+        role: 'CTO @ Rocketseat',
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galeraa 👋' },
+        {
+          type: 'paragraph',
+          content:
+            'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀,',
+        },
+        { type: 'link', content: '<a href="#">#novoprojeto</a>' },
+        { type: 'link', content: '<a href="#">#nlw</a>' },
+        { type: 'link', content: '<a href="#">#rocketseat</a>' },
+      ],
+    },
+  },
+];
 
 export function App() {
   return (
@@ -14,18 +38,11 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Pedro Geramano"
-            content="Frontend Developer"
-          />
+          <Post author="Pedro Geramano" content="Frontend Developer" />
 
-          <Post
-            author="Tiago Alves"
-            content="Design UI"
-          />
+          <Post author="Tiago Alves" content="Design UI" />
         </main>
       </div>
     </>
-  )
+  );
 }
-
