65be358c70669ced15b24cccc1b794b6ab5fca89
Pedro Germano <pedrogermano232@gmail.com>
2023-03-14 10:33:24 -0300

    key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -54,10 +54,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
-          } else if (line.type === 'link') {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.content === 'link') {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -82,7 +82,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
