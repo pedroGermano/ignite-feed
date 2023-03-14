2419cd8689ae9e5c93b304a80bb3139e53367d37
Pedro Germano <pedrogermano232@gmail.com>
2023-03-14 09:40:02 -0300

    use state

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,6 +10,8 @@ import styles from './Post.module.css';
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState(['Post muito bacana']);
 
+  const [newCommentText, setNewCommentText] = useState('');
+
   const publishedDateFormatted = format(publishedAt, `d 'de' LLLL 'às' HH:mm`, {
     locale: ptBr,
   });
@@ -21,8 +23,13 @@ export function Post({ author, publishedAt, content }) {
 
   function handleCreateNewComment(e) {
     e.preventDefault();
-    const newCommentText = e.target.comment.value;
+
     setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -61,7 +68,12 @@ export function Post({ author, publishedAt, content }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea name="comment" placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+          placeholder="Deixe um comentário"
+        />
 
         <footer>
           <button type="submit">Publicar</button>
